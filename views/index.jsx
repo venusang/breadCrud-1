199ba9cdd59ae736a7137136f6e52045f5105e48
Aram Hammoudeh <aramhammoudeh@Arams-MacBook-Pro.local>
2022-05-26 19:05:32 -0600

    catch up

--- views/index.jsx
+++ views/index.jsx
@@ -2,12 +2,21 @@ const React = require("react");
 const Default = require("./layouts/Default");
 
 function Index({ breads }) {
+  //   console.log("--------------------");
+  //   // FOCUS HERE
+  //   console.log(breads[0].name);
+
+  //   // STOP FOCUS HERE
+  //   console.log("--------------------");
+
   return (
     <Default>
       <h2>Index Page For the bread</h2>
       {/* <p>I have {breads[0].name} bread! </p> */}
       <ul>
         {breads.map((bread, index) => {
+          console.log(bread.name);
+          console.log(index);
           return (
             <li key={index}>
               <a href={`/breads/${index}`}>{bread.name}</a>
