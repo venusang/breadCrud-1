75d5500e339630a1afc8cbaa2bd57b509e1d3dec
Aram Hammoudeh <aramhammoudeh@Arams-MacBook-Pro.local>
2022-06-05 11:56:19 -0600

    add test

---
+++ test.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String },
+  password: String, // Shorthand for {type: String}
+});
+
+const User = mongoose.model("User", userSchema);
+
+console.log(User);
