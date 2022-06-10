4f4820c8e780105ba0eb4225a2a364e15a60d5a6
Venus Ang <venus@users.noreply.github.com>
2022-06-09 18:47:33 -0700

    Added baker model

---
+++ models/baker.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+
+const bakerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    enum: ['Jackie', 'Liam', 'Mama']
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  bio: String
+});
+
+const Baker = mongoose.model('Baker', bakerSchema);
+module.exports = Baker;
